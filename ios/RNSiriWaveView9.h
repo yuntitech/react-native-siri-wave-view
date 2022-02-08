b26c2933d0c185260c6c3bc464ec07f5d2e739e1
leejunhui <leejunhui2015@gmail.com>
2022-02-08 14:12:19 +0800

    fix: 修复 header map 导致的编译失败问题

--- ios/RNSiriWaveView9.h
+++ ios/RNSiriWaveView9.h
@@ -1,10 +1,4 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTViewManager.h"
-#else
 #import <React/RCTViewManager.h>
-#endif
-
 #import "PXSiriWave.h"
 
 @interface RNSiriWaveView9 : RCTViewManager
